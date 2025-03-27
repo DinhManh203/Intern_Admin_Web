d6252008cd1e72823bb0b647e99a7a68c1810905
DinhManh203 <128179148+DinhManh203@users.noreply.github.com>
2025-03-27 16:00:31 +0700

    2snd_update

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { assets } from '../assets/assets';
+
+const Sidebar = () => {
+  return (
+    <div className='w-[15%] min-h-screen bg-white text-white border-r p-4'>
+      <div className='flex flex-col gap-4 pt-4 text-[15px]'>
+        <NavLink 
+          to='/add' 
+          className={({ isActive }) => `
+            flex items-center gap-3 px-4 py-3 rounded-lg transition-all duration-300 
+            ${isActive ? 'bg-gray-300 text-white' : 'text-gray-400 hover:bg-gray-400 hover:text-white'}
+          `}
+        >
+          <img src={assets.add_icon} alt="Add" className="w-5 text-white" />
+          <p className='hidden md:block'>Add Items</p>
+        </NavLink>
+
+        <NavLink 
+          to='/list' 
+          className={({ isActive }) => `
+            flex items-center gap-3 px-4 py-3 rounded-lg transition-all duration-300 
+            ${isActive ? 'bg-gray-400 text-white' : 'text-gray-400 hover:bg-gray-400 hover:text-white'}
+          `}
+        >
+          <img src={assets.order_icon} alt="List" className="w-5" />
+          <p className='hidden md:block'>List Items</p>
+        </NavLink>
+
+        <NavLink 
+          to='/orders' 
+          className={({ isActive }) => `
+            flex items-center gap-3 px-4 py-3 rounded-lg transition-all duration-300 
+            ${isActive ? 'bg-gray-400 text-white' : 'text-gray-400 hover:bg-gray-400 hover:text-white'}
+          `}
+        >
+          <img src={assets.order_icon} alt="Orders" className="w-5" />
+          <p className='hidden md:block'>Orders</p>
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
